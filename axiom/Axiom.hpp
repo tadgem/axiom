ddfca2f7fe2f97e882184df3508a7862ef96b305
LiamDevlin <liamdevlin2305@gmail.com>
2026-06-21 20:46:50 +0100

    Update Axiom.hpp

--- axiom/Axiom.hpp
+++ axiom/Axiom.hpp
@@ -4,9 +4,10 @@
 #include "Log.hpp"
 #include "Maths.hpp"
 #include "Prim.hpp"
+#include "Vertex.hpp"
+#include "Buffer.hpp"
 #include "Utils.hpp"
 
 // Third party includes
 // TODO: (Do we want these in the public header?)
-
 #include "imgui.h"

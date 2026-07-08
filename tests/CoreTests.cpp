f8ae4f9b88c3f39512ca7fb0055814b8cef13f5a
LiamDevlin <liamdevlin2305@gmail.com>
2026-07-08 13:48:49 +0100

    Remove example tests + add new  test to ensure assets can be loaded.

--- tests/CoreTests.cpp
+++ tests/CoreTests.cpp
@@ -3,21 +3,14 @@
 using namespace axm;
 
 
-TestResult ExampleTest(AppState *e) { return TestResult::Pass(); }
-
-TestResult ExampleTest2(AppState *e) { return TestResult::Fail("Something bad happened"); }
-
-TestResult ExampleTest3(AppState *e) { return TestResult::Pass(); }
-
-
-TestResult AssetManager_CanProvideFactory(AppState *e) {
+TestResult AssetManager_CanProvideFactory(AppState* e) {
 
     using TestBinaryAsset = AssetT<u64, AssetType::Binary>;
 
 
-    auto load = [](const String &path) { return AssetLoadResult{.m_Next = AXM_NEW(TestBinaryAsset, path, 2305)}; };
-    auto unload = [](Asset *a) {
-        auto *b = dynamic_cast<TestBinaryAsset *>(a);
+    auto load = [](const String& path) { return AssetLoadResult { .m_Next = AXM_NEW(TestBinaryAsset, path, 2305) }; };
+    auto unload = [](Asset* a) {
+        auto* b = dynamic_cast<TestBinaryAsset*>(a);
         AXM_DELETE(b);
     };
 
@@ -27,19 +20,46 @@ TestResult AssetManager_CanProvideFactory(AppState *e) {
     while (e->m_AssetManager.AnyAssetsLoading()) {
         e->m_AssetManager.Update();
     }
-    auto *binary = e->m_AssetManager.GetAsset<TestBinaryAsset>("test_resources/test.bin");
+    auto* binary = e->m_AssetManager.GetAsset<TestBinaryAsset>("test_resources/test.bin");
 
     AXM_TEST_ASSERT(binary->m_Data == 2305, "BinaryAsset asset factory not invoked!");
 
     return TestResult::Pass();
 }
 
+TestResult AssetManager_CanLoadAsset(AppState* e) {
+
+    using TestBinaryAsset = AssetT<Vector<u8>, AssetType::Binary>;
+
+    auto load = [](const String& path) {
+        auto bin = Utils::LoadBinaryFromPath(path);
+        return AssetLoadResult { .m_Next = AXM_NEW(TestBinaryAsset, path, std::move(bin)) };
+    };
+    auto unload = [](Asset* a) {
+        auto* b = dynamic_cast<TestBinaryAsset*>(a);
+        AXM_DELETE(b);
+    };
+
+    e->m_AssetManager.ProvideAssetFactory(AssetType::Binary, load, unload);
+    e->m_AssetManager.LoadAsset("test_resources/test.bin", AssetType::Binary);
+
+    while (e->m_AssetManager.AnyAssetsLoading()) {
+        e->m_AssetManager.Update();
+    }
+
+    auto* binary = e->m_AssetManager.GetAsset<TestBinaryAsset>("test_resources/test.bin");
+
+    AXM_TEST_ASSERT(binary->m_Data.size() == 16, "Binary Asset not expected size");
+    AXM_TEST_ASSERT(binary->m_Data[0] == '0', "Expected first binary blob to be 0");
+    AXM_TEST_ASSERT(binary->m_Data[15] == '1', "Expected last binary blob to be 1");
+
+
+    return TestResult::Pass();
+}
+
 AXM_BEGIN_TESTS("Core Tests")
 
-AXM_ADD_TEST(ExampleTest)
-AXM_ADD_TEST(ExampleTest2)
-AXM_ADD_TEST(ExampleTest3)
-
 AXM_ADD_TEST(AssetManager_CanProvideFactory)
+AXM_ADD_TEST(AssetManager_CanLoadAsset)
 
 AXM_END_TESTS()
